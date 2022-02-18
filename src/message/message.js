bd1830fe8f10900453903f1a1300f77585a50ae9
Maximilian234 <vipzone2006@yandex.ru>
2022-02-18 12:22:15 +0300

    com2

--- src/message/message.js
+++ src/message/message.js
@@ -1,13 +1,80 @@
-import "./style.css"
+import { useState, useEffect} from "react";
+import "./style.css";
 
+export const App = () => {
+  const [value, setValue] = useState("");
+  const [messagelist, setMessagelist] = useState([]);
+  // messagelist - это переменная массива, которую хочу использовать.
+  // setMessagelist - это функция, для изменения значения массива messageList
+  // useState - это функция для изменения состояния. возвращает массив; 
+  // messagelist - это значение по умолчанию для переменной состояния. Как обычная переменная   
+  // Вызывая функцию setMessagelist для изменения значения messagelist в используемом компоненте, например handleButton, будет повторно активирован хуком useState([]) и начнет заново отображаться с новым значением messagelist
+  
 
-export const Message = ({myname}) => {
+  useEffect(() => {
+    let timeOutBot = null;
+    const lastmessage = messagelist[messagelist.length-1];
+
+    if(lastmessage?.user !== "Bot" && messagelist.length) {
+      console.log('ok')
+        timeOutBot = setTimeout(()=> {
+        setMessagelist([...messagelist, { user: "Bot", value: "Hello i'm Bot"}]);
+      }, 2500);
+    }
+    return () => clearInterval(timeOutBot);
+  
+  },[messagelist]);
+
+  const handleClick = (event) => {
+    setValue(event.target.value);
+  };
+
+  const nahdleKey = (event) => {
+    if (event.code === "Enter") {
+      handleButton();
+    }
+  };
+
+  const handleButton = () => {
+    if (value) {
+      setMessagelist([...messagelist, { user: "Maxim Mescheryakov", value }]);
+      setValue("");
+    } else {
+      alert("error");
+    }
+  };
+
+  
+
   return (
     <div className="container">
-      <h1 className="stH1"> Это сообщение от Message</h1>
-      <h2 className="stH1">Мое имя {myname}</h2>
+      <div className="containerBox">
+        <div className="boxChat">
+          {messagelist.map((item, keyId) => {
+            return (
+              <div key={keyId}>
+                <p className="p">{item.user}</p>
+                <p className="p">{item.value}</p>
+                {/* <p className="p">{item.bot}</p> */}
+              </div>
+           
+            );
+          })}
+        </div>
+        <div className="boxInput">
+          <input
+            className="input"
+            value={value}
+            onKeyPress={nahdleKey}
+            onChange={handleClick}
+          />
+          <button className="button" type={"button"} onClick={handleButton}>
+            Отправить
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Message;
+export default App;
