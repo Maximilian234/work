b7297ce2714382cf56ee8b09e602d7d7d3692ce7
Maximilian234 <vipzone2006@yandex.ru>
2022-02-22 17:56:32 +0300

    commit3

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./message";
+import {createTheme } from "@mui/material";
+import {ThemeProvider} from "@emotion/react";
 
 
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App/>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/message/message.js
+++ src/message/message.js
@@ -1,29 +1,49 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import "./style.css";
+import "./common";
+import { AUTHOR } from "./common";
+import {
+  Avatar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  TextField,
+} from "@mui/material";
+import { AccountCircle, Android, Chat, Send } from "@mui/icons-material";
+import Chtaroom from "../list/list";
+// import { render } from "react-dom";
+// import { propsToClassKey } from "@mui/styles";
 
 export const App = () => {
   const [value, setValue] = useState("");
   const [messagelist, setMessagelist] = useState([]);
   // messagelist - это переменная массива, которую хочу использовать.
   // setMessagelist - это функция, для изменения значения массива messageList
-  // useState - это функция для изменения состояния. возвращает массив; 
-  // messagelist - это значение по умолчанию для переменной состояния. Как обычная переменная   
+  // useState - это функция для изменения состояния. возвращает массив;
+  // messagelist - это значение по умолчанию для переменной состояния. Как обычная переменная
   // Вызывая функцию setMessagelist для изменения значения messagelist в используемом компоненте, например handleButton, будет повторно активирован хуком useState([]) и начнет заново отображаться с новым значением messagelist
-  
+
+  const Elem = (props) => {
+    return <h2> {props.render("Выбрать чат")} </h2>;
+  };
 
   useEffect(() => {
     let timeOutBot = null;
-    const lastmessage = messagelist[messagelist.length-1];
+    const lastmessage = messagelist[messagelist.length - 1];
 
-    if(lastmessage?.user !== "Bot" && messagelist.length) {
-      console.log('ok')
-        timeOutBot = setTimeout(()=> {
-        setMessagelist([...messagelist, { user: "Bot", value: "Hello i'm Bot"}]);
+    if (lastmessage?.user !== AUTHOR.bot && messagelist.length) {
+      console.log("ok");
+      timeOutBot = setTimeout(() => {
+        setMessagelist([
+          ...messagelist,
+          { user: AUTHOR.bot, value: AUTHOR.textBot },
+        ]);
       }, 2500);
     }
     return () => clearInterval(timeOutBot);
-  
-  },[messagelist]);
+  }, [messagelist]);
 
   const handleClick = (event) => {
     setValue(event.target.value);
@@ -37,42 +57,70 @@ export const App = () => {
 
   const handleButton = () => {
     if (value) {
-      setMessagelist([...messagelist, { user: "Maxim Mescheryakov", value }]);
+      setMessagelist([
+        ...messagelist,
+        {
+          user: AUTHOR.user,
+          value,
+        },
+      ]);
       setValue("");
     } else {
       alert("error");
     }
   };
 
-  
-
   return (
     <div className="container">
       <div className="containerBox">
+        
         <div className="boxChat">
-          {messagelist.map((item, keyId) => {
-            return (
-              <div key={keyId}>
-                <p className="p">{item.user}</p>
-                <p className="p">{item.value}</p>
-                {/* <p className="p">{item.bot}</p> */}
-              </div>
-           
-            );
-          })}
+          <List
+            sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+          >
+            {messagelist.map((item, keyId) => {
+              return (
+                <ListItem key={keyId}>
+                  <ListItemAvatar>
+                    <Avatar>
+                      {item.user === AUTHOR.user ? (
+                        <AccountCircle />
+                      ) : (
+                        <Android />
+                      )}
+                    </Avatar>
+                    <ListItemText>{item.value}</ListItemText>
+                  </ListItemAvatar>
+                </ListItem>
+              );
+            })}
+          </List>
         </div>
         <div className="boxInput">
-          <input
-            className="input"
+          <TextField
+            autoFocus
+            color="secondary"
+            id="standard-basic"
+            label="Standard"
+            variant="standard"
             value={value}
             onKeyPress={nahdleKey}
             onChange={handleClick}
+            fullWidth
           />
-          <button className="button" type={"button"} onClick={handleButton}>
-            Отправить
-          </button>
+
+          <IconButton className="button" type={"button"} onClick={handleButton}>
+            <Send />
+          </IconButton>
         </div>
       </div>
+      <Elem
+        render={(num) => {
+          return <div>{num}</div>;
+        }}
+      />
+      <div children={<Chat />} />
+      <Chtaroom/>
     </div>
   );
 };
